5651ebc43112c39e4b941d470338ba40cc83d69d
Anirudh Kulkarni <anirudh.kulkarni@lumenradio.com>
2025-10-04 12:07:44 +0200

    fetch data using environment credentials

--- fetch_data.py
+++ fetch_data.py
@@ -1,6 +1,30 @@
 import earthaccess
 
-username = "ultseid"
-pw = "4d&vcVH9q@AT_FY"
+# EARTHDATA_USERNAME="ultseid"
+# EARTHDATA_PASSWORD="4d&vcVH9q@AT_FY"
 
-earthaccess.login()
+earthaccess.login(strategy="environment")
+
+results = earthaccess.search_datasets(
+    keyword="icesat-2"
+    )
+
+print("#########################################################")
+print("#########################################################")
+print("#########################################################")
+print("#########################################################")
+
+print(len(results))
+
+datasets = earthaccess.search_datasets(
+    keyword="modis",
+    bounding_box=(11°37'32.5"E, )
+    cloud_hosted=True
+)
+
+print(len(datasets))
+
+
+
+# Download dataset
+# earthaccess.download()
